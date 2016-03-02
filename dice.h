438ef7a061c9d17daa1187aa383faae003b2b713
Kevin Hublou <kevinhublou@gmail.com>
2016-03-01 22:03:34 -0500

    Setup project in CLion

--- dice.h
+++ dice.h
@@ -1,5 +1,13 @@
-/* Basic dice rolling functions for Dungeon Utility.
-*  Kevin Hublou
-*/
+//
+// Created by k on 3/1/16.
+//
+
+#ifndef DUNGEONUTILITY_DICE_H
+#define DUNGEONUTILITY_DICE_H
 
 #include <iostream>
+#include <stdlib.h>
+
+
+
+#endif //DUNGEONUTILITY_DICE_H
